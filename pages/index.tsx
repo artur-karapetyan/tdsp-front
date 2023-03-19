30a17f2dd43edf65b5d8052eed13d386db9820a1
Artur Karapetyan <artur.karapetyan.2002@gmail.com>
2023-03-19 18:27:54 +0400

    UPD:

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,43 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+function handleClick(event) {
+  if (event.target.value === "") {
+    event.target.value = "";
+  }
+}
+
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center w-full h-full overflow-hidden gap-2.5 px-[30px] py-2.5 bg-white">
-        <div className="flex flex-col justify-center items-center flex-grow-0 flex-shrink-0 w-[600px] relative overflow-hidden gap-10 p-10 rounded-3xl bg-[#deecfa]">
-          <input className="self-stretch flex-grow-0 flex-shrink-0 w-[520px] h-16 relative overflow-hidden rounded-3xl bg-white" />
-          <div className="self-stretch flex-grow-0 flex-shrink-0 w-[520px] h-16 relative overflow-hidden rounded-3xl bg-white" />
-          <div className="flex-grow-0 flex-shrink-0 w-[245px] h-16 relative overflow-hidden rounded-3xl bg-[#9db3ed]" />
+      <div className="flex flex-col justify-start items-center w-full h-full relative gap-10 px-[60px] py-[100px] bg-neutral-100">
+        <p className="flex-grow-0 flex-shrink-0 w-[573px] h-[227px] text-8xl text-center text-[#3193f5]">
+          <span className="flex-grow-0 flex-shrink-0 w-[573px] h-[227px] text-8xl text-center text-[#3193f5]">
+            The
+          </span>
+          <br />
+          <span className="flex-grow-0 flex-shrink-0 w-[573px] h-[227px] text-8xl text-center text-[#3193f5]">
+            Interns
+          </span>
+        </p>
+        <div className="flex flex-col justify-center items-center flex-grow-0 flex-shrink-0 h-[300px] w-[400px] gap-[30px] px-[35px] py-[30px] rounded-3xl bg-[#deecfa]">
+          <input
+            className="flex justify-start text-2xl text-left text-[#3193f5] items-center self-stretch flex-grow relative overflow-hidden gap-2.5 px-[27px] py-[7px] rounded-[20px] bg-neutral-100"
+            type="text"
+            placeholder="Email"
+            onClick={handleClick}
+          ></input>
+          <input
+            className="flex justify-start text-2xl text-left text-[#3193f5] items-center self-stretch flex-grow relative overflow-hidden gap-2.5 px-[27px] py-[7px] rounded-[20px] bg-neutral-100"
+            type="password"
+            placeholder="Password"
+            onClick={handleClick}
+          ></input>
+          <button className="flex justify-center items-center flex-grow w-[139px] relative overflow-hidden gap-2.5 px-7 py-2 rounded-3xl bg-[#9db3ed] hover:bg-[#7295f3] transition-colors duration-500">
+            <p className="flex-grow-0 flex-shrink-0 text-[32px] text-center text-neutral-100">
+              Login
+            </p>
+          </button>
         </div>
       </div>
     </>
