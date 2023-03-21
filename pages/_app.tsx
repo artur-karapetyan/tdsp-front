2e72229a4fff51abcae688ad9bb1a9b4414034d9
Artur Karapetyan <artur.karapetyan.2002@gmail.com>
2023-03-21 17:05:45 +0400

    UPD:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({ subsets: ["latin"], variable: "--josefin" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} className={josefin.className} />;
 }
